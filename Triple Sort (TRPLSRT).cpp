0ae93c59a2d5b3fd88eb263fdaa4286998a621e6
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-06-08 22:19:05 +0530

    Update Triple Sort (TRPLSRT).cpp

--- Triple Sort (TRPLSRT).cpp
+++ Triple Sort (TRPLSRT).cpp
@@ -1,21 +1,22 @@
 #include <bits/stdc++.h>
 #define MOD 1000000007
-using namespace std;
 #include <tuple>
+using namespace std;
 int n, k;
-int arr[200001];
-bool visited[200001];
+int arr[200005];
+bool visited[200005];
 
-void solve(){
+void solve(int test){
     cin>>n>>k;
     vector<pair<int, int> > doub;
     vector<tuple<int, int, int> > answer;
     for(int i=1; i<=n; i++) cin>>arr[i];
-    memset(visited, 0, (n+1)*sizeof(bool));
-    vector<int> cycle;
+    for(int i=1; i<=n; ++i) visited[i] = false;
+
     for(int i=1; i<=n; i++){
+        vector<int> cycle;
         if(!visited[i]){
-            int j=1;
+            int j=i;
             while(!visited[j]){
                 visited[j] = true;
                 cycle.push_back(j);
@@ -30,11 +31,11 @@ void solve(){
             int x = cycle.back();
             if(cycle.size()==1){
                 cycle.pop_back();
-                answer.push_back(tie(x, y, z));
-                k--;
             }
+            answer.push_back(tie(x, y, z));
+            k--;
         }
-        if(cycle.size()==2) doub.push_back({cycle[0], cycle[1]});
+        if(cycle.size()==2) doub.push_back(make_pair(cycle[0], cycle[1]));
     }
     while(doub.size()>1){
             pair<int, int> one = doub.back();
@@ -45,14 +46,15 @@ void solve(){
             answer.push_back(tie(one.first, one.second, sec.first));
             k-=2;
         }
-        if(!doub.empty()) cout<<"-1"<<endl;
-        if(k>=0){
+        if(!doub.empty()) k=-1;
+        if(k<0)
+            cout<<"-1\n";
+        else{
             cout<<answer.size()<<endl;
             for(int i=0; i<answer.size(); ++i){
                 cout<<get<0>(answer[i])<<" "<<get<1>(answer[i])<<" "<<get<2>(answer[i])<<endl;
             }
         }
-        else cout<<"-1"<<endl;
         return;
 }
 
@@ -60,8 +62,8 @@ int main()
 {
     int t;
     cin>>t;
-    while(t--){
-        solve();
+    for(int i=0; i<t; i++){
+        solve(i);
     }
     return 0;
 }
