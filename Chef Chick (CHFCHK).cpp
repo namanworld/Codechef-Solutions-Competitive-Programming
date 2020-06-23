2c37d992c35a3f62bf2e4d88dfcf5799887d5323
Naman Jain <nmnjn2345@gmail.com>
2020-06-23 12:41:00 +0530

    Create Chef Chick (CHFCHK).cpp

---
+++ Chef Chick (CHFCHK).cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define int long long
+#define endl '\n'
+#define MOD 1000000007
+#define INF 10000000000000000
+// find_by_order(), order_of_key()
+#define oset tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define FOR(i, n) for(int i=0; i<n; i++)
+#define all(a) a.begin(),a.end()
+#define mp make_pair
+#define vi vector<int>
+#define ss second
+#define pb push_back
+#define ff first
+#define pii pair<int,int>
+#define vii vector<pii>
+#define pq priority_queue<int>
+#define pdq priority_queue<int, vi, greater<int> >
+#define gethash(l, r) (MOD-(h[r+1]*p_pow[r-l+1])%MOD+h[l])%MOD;
+
+using namespace __gnu_pbds;
+using namespace std;
+
+const int N = 105;
+int arr[N];
+
+void solve(int test){
+    int n;
+    cin>>n;
+    FOR(i, n) cin>>arr[i];
+    sort(arr, arr+n);
+    int dist = arr[0];
+    for(int i=1; i<n; i++) {
+        int temp = INT_MAX;
+        for(int j=0; j<i; j++){
+            temp = min(temp, arr[i]%arr[j]);
+        }
+        dist = max(dist, temp);
+    }
+    cout<<dist<<endl;
+}
+
+signed main()
+{
+    fastIO
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        solve(i);
+    }
+    return 0;
+}
