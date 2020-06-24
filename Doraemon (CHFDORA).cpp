21ce632ba813683b6e04bb60a83ea867dba55acb
Naman Jain <nmnjn2345@gmail.com>
2020-06-24 06:25:15 +0530

    Create Doraemon (CHFDORA).cpp

---
+++ Doraemon (CHFDORA).cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define int long long
+#define endl '\n'
+#define MOD 1000000007
+#define INF 10000000000000000
+// find_by_order(), order_of_key()
+#define oset tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define FOR(i, n) for(int i=0; i<n; i++)
+#define all(a) a.begin(),a.end()
+#define mp make_pair
+#define vi vector<int>
+#define ss second
+#define pb push_back
+#define ff first
+#define pii pair<int,int>
+#define vii vector<pii>
+#define pq priority_queue<int>
+#define pdq priority_queue<int, vi, greater<int> >
+#define gethash(l, r) (MOD-(h[r+1]*p_pow[r-l+1])%MOD+h[l])%MOD;
+
+using namespace __gnu_pbds;
+using namespace std;
+
+const int N = 105;
+
+void solve(int test){
+    int n, m;
+    cin>>n>>m;
+    int a[n][m];
+
+    FOR(i, n){
+        FOR(j, m) cin>>a[i][j];
+    }
+
+    int ans = 0;
+
+    FOR(i, n){
+        FOR(j, m){
+            int w,x,y,z;
+            w = x = i;
+            y = z = j;
+
+            int count = 0;
+            while( w>=0 && x < n && y>=0 && z < m && a[w][j]==a[x][j] && a[i][y]==a[i][z]){
+                w--;
+                x++;
+                y--;
+                z++;
+                count++;
+            }
+            ans+=count;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+signed main()
+{
+    fastIO
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        solve(i);
+    }
+    return 0;
+}
