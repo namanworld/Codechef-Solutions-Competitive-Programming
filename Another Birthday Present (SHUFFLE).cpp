00ecb71426ad5be7ae917bbf47a0897564f61303
Naman Jain <nmnjn2345@gmail.com>
2020-06-29 14:27:07 +0530

    Create Another Birthday Present (SHUFFLE).cpp

---
+++ Another Birthday Present (SHUFFLE).cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define int long long
+#define endl '\n'
+#define MOD 1000000007
+#define INF 10000000000000000
+// find_by_order(), order_of_key()
+#define oset tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define FORI(i,j,n) for(int i=j; i<n; i++)
+#define FOR(i, n) FORI(i, 0, n)
+#define all(a) a.begin(),a.end()
+#define mp make_pair
+#define vi vector<int>
+#define ss second
+#define pb push_back
+#define ff first
+#define pii pair<int,int>
+#define vii vector<pii>
+#define pq priority_queue<int>
+#define pdq priority_queue<int, vi, greater<int> >
+#define gethash(l, r) (MOD-(h[r+1]*p_pow[r-l+1])%MOD+h[l])%MOD;
+
+using namespace __gnu_pbds;
+using namespace std;
+
+const int N = 1e5+4;
+int arr[N];
+
+void solve(int test){
+    int n, k; cin>>n>>k;
+    vector<vi> list(k);
+    FOR(i, n) {
+        cin>>arr[i];
+        list[i%k].pb(arr[i]);
+    }
+    FOR(i, k) {
+        sort(all(list[i]));
+        FOR(j, list[i].size()) arr[j*k+i] = list[i][j];
+    }
+
+    bool flag = false;
+    FOR(i, n-1){
+        if(arr[i]>arr[i+1]){
+            flag = true;
+            break;
+        }
+    }
+    if(flag) cout<<"no"<<endl;
+    else cout<<"yes"<<endl;
+}
+
+signed main()
+{
+    fastIO
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        solve(i);
+    }
+    return 0;
+}
+
